d4ec9240c8c25f759683220c7f90cc9e953799f9
Coffandro <macrosedk@gmail.com>
2024-01-29 08:02:43 +0100

    uwu

--- RPlayer_ManagerGui.py
+++ RPlayer_ManagerGui.py
@@ -13,6 +13,7 @@ ComPort = ""
 
 settings = Settings.LoadSettings()
 
+
 # SETTING UP TABVIEW CLASS
 class MyTabView(customtkinter.CTkTabview):
     def __init__(self, master, **kwargs):
@@ -23,9 +24,9 @@ class MyTabView(customtkinter.CTkTabview):
         tab1 = self.add("General settings")
         tab2 = self.add("Primary mode")
         tab3 = self.add("Secondary mode")
-        
+
         # add 'master=self.tab("Primary mode")' or 'master=self.tab("Secondary mode")' to any master
-        
+
         # general settings
         def switch_event(self):
             global settings
@@ -39,12 +40,12 @@ class MyTabView(customtkinter.CTkTabview):
         def optionmenu_callback01(choice):
             global settings
             global ComPort
-            
+
             print("optionmenu02 dropdown clicked:", choice)
             settings = {
-                "comport" : OptionsList01Short[OptionsList01.index(choice)],
+                "comport": OptionsList01Short[OptionsList01.index(choice)],
                 "optionmenu1": self.optionmenu02.get(),
-                "optionmenu2": self.optionmenu03.get()
+                "optionmenu2": self.optionmenu03.get(),
             }
             print(settings)
 
@@ -55,15 +56,16 @@ class MyTabView(customtkinter.CTkTabview):
             command=lambda: switch_event(self),
             variable=self.switch_var,
             onvalue=True,
-            offvalue=False)
+            offvalue=False,
+        )
         self.switch.select()
-        
+
         OptionsList01 = PortsCom.GetSerialPorts()
         OptionsList01Short = PortsCom.GetShortenedSerialPorts()
-        
-        #for i in PortsCom.GetSerialPorts():
+
+        # for i in PortsCom.GetSerialPorts():
         #    OptionsList01.append(i)
-        #for i in PortsCom.GetShortenedSerialPorts():
+        # for i in PortsCom.GetShortenedSerialPorts():
         #    OptionsList01Short.append(i)
 
         self.optionmenu_var01 = customtkinter.StringVar(value=OptionsList01[-1])
@@ -71,89 +73,88 @@ class MyTabView(customtkinter.CTkTabview):
             master=self.tab("General settings"),
             command=optionmenu_callback01,
             values=OptionsList01,
-            variable=self.optionmenu_var01)
+            variable=self.optionmenu_var01,
+        )
 
         self.optionmenu01.grid(row=1, column=1, padx=20, pady=10)
         self.switch.grid(row=0, column=0, padx=20, pady=10)
-        
-        
+
         # Primary menu
         def optionmenu_callback02(choice):
             global settings
             print("optionmenu02 dropdown clicked:", choice)
             settings = {
-                "comport" : ComPort,
+                "comport": ComPort,
                 "optionmenu1": self.optionmenu02.get(),
-                "optionmenu2": self.optionmenu03.get()
+                "optionmenu2": self.optionmenu03.get(),
             }
             print(settings)
-        
-        
+
         self.optionmenu_var02 = customtkinter.StringVar(value=settings["optionmenu1"])
         self.optionmenu02 = customtkinter.CTkOptionMenu(
             master=self.tab("Primary mode"),
-            values=[
-                "Media info",
-                "Option 2",
-                "Option 3"
-            ],
+            values=["Media info", "Option 2", "Option 3"],
             command=optionmenu_callback01,
-            variable=self.optionmenu_var02)
-        
+            variable=self.optionmenu_var02,
+        )
+
         self.optionmenu02.grid(row=0, column=0, padx=20, pady=10)
-        
+
         # Second Menu
-        
+
         def optionmenu_callback03(choice):
             global settings
             print("optionmenu03 dropdown clicked:", choice)
             settings = {
-                "comport" : ComPort,
+                "comport": ComPort,
                 "optionmenu1": self.optionmenu02.get(),
-                "optionmenu2": self.optionmenu03.get()
+                "optionmenu2": self.optionmenu03.get(),
             }
             print(settings)
 
         self.optionmenu_var03 = customtkinter.StringVar(value=settings["optionmenu2"])
         self.optionmenu03 = customtkinter.CTkOptionMenu(
             master=self.tab("Secondary mode"),
-            values=[
-                "Media info",
-                "option 2",
-                "option 3"
-            ],
+            values=["Media info", "option 2", "option 3"],
             command=optionmenu_callback03,
-            variable=self.optionmenu_var03)
-        
+            variable=self.optionmenu_var03,
+        )
+
         self.optionmenu03.grid(row=0, column=0, padx=20, pady=10)
+
 
 # SETTING UP APP
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        
+
         self.title("Rose Player Management app")
         self.iconbitmap("Icons/Rose256.ico")
-    
+
         self.tab_view = MyTabView(master=self)
         self.tab_view.grid(row=0, column=0, padx=20, pady=20, columnspan=2)
-        
-        self.ApplyButton = customtkinter.CTkButton(self, text="Apply settings", command=self.apply_button_callbck)
+
+        self.ApplyButton = customtkinter.CTkButton(
+            self, text="Apply settings", command=self.apply_button_callbck
+        )
         self.ApplyButton.grid(row=1, column=0, padx=20, pady=20)
-        
-        self.SaveButton = customtkinter.CTkButton(self, text="Save settings", command=self.save_button_callbck)
+
+        self.SaveButton = customtkinter.CTkButton(
+            self, text="Save settings", command=self.save_button_callbck
+        )
         self.SaveButton.grid(row=1, column=1, padx=20, pady=20)
-        
+
     def save_button_callbck(self):
         global settings
-    
+
     def apply_button_callbck(self):
         global settings
         print(settings)
-        with open('Settings.json', 'w') as f:
+        with open("Settings.json", "w") as f:
             json.dump(settings, f)
         subprocess.run(["python", "RPlayer_ControllerCom.py"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = App()
     app.mainloop()
